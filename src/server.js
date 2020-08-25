b81a85bfa19960b39c1c437920f4c2a01ac7210c
Justin Vaughn <jtv6445@g.rit.edu>
2020-08-25 18:31:12 -0400

    simple http server completed

--- src/server.js
+++ src/server.js
@@ -1,16 +1,40 @@
 const http = require('http');
-const fs = require('fs');
-
-// const hello = 'hi';
+const htmlHandler = require('./htmlResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+const pngHandler = require('./pngResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const onRequest = (request, response) => {
   console.log(request.url);
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end();
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
+      break;
+    case '/dankmemes':
+      pngHandler.getSpongegar(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port);
